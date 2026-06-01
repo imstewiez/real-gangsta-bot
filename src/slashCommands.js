e8ae2b6b97825408faa53b01129d54ffa0e6e26d
imstewiez <steven.southan97@gmail.com>
2026-06-01 07:15:58 +0100

    Add Discord kick command for Patrao

--- src/slashCommands.js
+++ src/slashCommands.js
@@ -12,6 +12,12 @@ const commands = [
     .addIntegerOption(opt => opt.setName('id').setDescription('ID de uma saida especifica').setRequired(false)),
 
   new SlashCommandBuilder()
+    .setName('kick')
+    .setDescription('Expulsar um membro do bairro')
+    .addUserOption(opt => opt.setName('membro').setDescription('Membro a expulsar').setRequired(true))
+    .addStringOption(opt => opt.setName('motivo').setDescription('Motivo').setRequired(false)),
+
+  new SlashCommandBuilder()
     .setName('primeira-vez')
     .setDescription('Guia rapido para usar a comunidade'),
 ];
