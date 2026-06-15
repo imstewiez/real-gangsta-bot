0e363ef69df64ac4ebcd17a15e4809a109ddf46e
imstewiez <steven.southan97@gmail.com>
2026-06-15 06:59:05 +0100

    Disable public slash commands

--- src/slashCommands.js
+++ src/slashCommands.js
@@ -1,24 +1,7 @@
 'use strict';
-const { SlashCommandBuilder } = require('discord.js');
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('meu-pedido')
-    .setDescription('Ver o estado do teu pedido de acesso'),
-
-  new SlashCommandBuilder()
-    .setName('perfil')
-    .setDescription('Ver o teu perfil básico na Ballas Gang'),
-
-  new SlashCommandBuilder()
-    .setName('sync-membro')
-    .setDescription('Sincronizar um membro entre DB e Discord')
-    .addUserOption(opt => opt.setName('membro').setDescription('Membro a sincronizar').setRequired(true)),
-
-  new SlashCommandBuilder()
-    .setName(['k', 'i', 'c', 'k'].join(''))
-    .setDescription('Ação administrativa')
-    .addUserOption(opt => opt.setName('user').setDescription('User').setRequired(true)),
-];
+// O bot funciona por botões e modais. Mantemos este array vazio para o boot
+// limpar comandos antigos registados no Discord através do PUT de guild commands.
+const commands = [];
 
 module.exports = { commands };
