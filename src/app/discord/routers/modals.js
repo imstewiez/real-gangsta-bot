060dd3da812b451cf74bdb79d134b94c2efd5c35
imstewiez <steven.southan97@gmail.com>
2026-06-15 08:27:33 +0100

    Route radio manual modal

--- src/app/discord/routers/modals.js
+++ src/app/discord/routers/modals.js
@@ -2,11 +2,13 @@
 
 const { handleTagModal, handleDenyModalSubmit } = require('../../../onboarding/onboardingHandlers');
 const { handleTropinhaModal } = require('../../../onboarding/tropinhaHandlers');
+const { handleRadioManualModal } = require('../../../radio/radioPanel');
 
 const exact = (id, handler) => ({ match: x => x === id, handler });
 const prefix = (p, handler) => ({ match: x => x.startsWith(p), handler });
 
 const MODAL_ROUTES = [
+  exact('radio::manual_modal', handleRadioManualModal),
   exact('onboard::modal_tropinha', handleTropinhaModal),
   prefix('onboard::modal_tag', handleTagModal),
   prefix('onboard::modal_deny::', handleDenyModalSubmit),
