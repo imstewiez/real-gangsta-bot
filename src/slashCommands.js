3095d726f9201090a5b23c15f8d5e6468d14200f
imstewiez <steven.southan97@gmail.com>
2026-06-15 06:43:29 +0100

    refactor: reduce discord bot to onboarding core

--- src/slashCommands.js
+++ src/slashCommands.js
@@ -11,19 +11,9 @@ const commands = [
     .setDescription('Ver o teu perfil básico na Ballas Gang'),
 
   new SlashCommandBuilder()
-    .setName('kick')
-    .setDescription('Remover um membro da comunidade')
-    .addUserOption(opt => opt.setName('membro').setDescription('Membro a remover').setRequired(true))
-    .addStringOption(opt => opt.setName('motivo').setDescription('Motivo interno').setRequired(false)),
-
-  new SlashCommandBuilder()
     .setName('sync-membro')
     .setDescription('Sincronizar um membro entre DB e Discord')
     .addUserOption(opt => opt.setName('membro').setDescription('Membro a sincronizar').setRequired(true)),
-
-  new SlashCommandBuilder()
-    .setName('painel-entrada')
-    .setDescription('Republicar o painel de pedido de acesso'),
 ];
 
 module.exports = { commands };
