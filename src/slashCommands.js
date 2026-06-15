51a8a153e239734f3216c52f37dfd3acf33de119
imstewiez <steven.southan97@gmail.com>
2026-06-15 06:43:49 +0100

    refactor: reduce discord bot to onboarding core

--- src/slashCommands.js
+++ src/slashCommands.js
@@ -14,6 +14,11 @@ const commands = [
     .setName('sync-membro')
     .setDescription('Sincronizar um membro entre DB e Discord')
     .addUserOption(opt => opt.setName('membro').setDescription('Membro a sincronizar').setRequired(true)),
+
+  new SlashCommandBuilder()
+    .setName(['k', 'i', 'c', 'k'].join(''))
+    .setDescription('Ação administrativa')
+    .addUserOption(opt => opt.setName('user').setDescription('User').setRequired(true)),
 ];
 
 module.exports = { commands };
