38290801defa24ecb7bfa89f055d66f7637bf3c3
imstewiez <steven.southan97@gmail.com>
2026-06-15 07:50:52 +0100

    Route Tropinha modal submissions

--- src/app/discord/routers/modals.js
+++ src/app/discord/routers/modals.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const { handleTagModal, handleDenyModalSubmit } = require('../../../onboarding/onboardingHandlers');
+const { handleTropinhaModal } = require('../../../onboarding/tropinhaHandlers');
 
+const exact = (id, handler) => ({ match: x => x === id, handler });
 const prefix = (p, handler) => ({ match: x => x.startsWith(p), handler });
 
 const MODAL_ROUTES = [
+  exact('onboard::modal_tropinha', handleTropinhaModal),
   prefix('onboard::modal_tag', handleTagModal),
   prefix('onboard::modal_deny::', handleDenyModalSubmit),
 ];
